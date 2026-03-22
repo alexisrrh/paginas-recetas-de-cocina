cb5b4350e2016d4291dc9577868739db5af899eb
Alexis Rodriguez <alexisrrh123@gmail.com>
2026-03-22 12:43:31 +0100

    actualizacion de modal

--- src/js/components/Card2.jsx
+++ src/js/components/Card2.jsx
@@ -2,27 +2,47 @@ import React, {useEffect, useState} from "react";
 
 const Card2 = ({receta,onVolver}) => {
 
-    
- return(
-        <div className="container-row text-center mt-5">
-            <div className="card mb-3" style={{width: 500}}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src={receta.image} className="img-fluid rounded-start" alt={receta.name}/>
+
+    return (
+        <div className="modal show fade d-block" tabIndex="-1">
+            <div className="modal-dialog modal-lg modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{receta.name}</h5>
+                        <button type="button" className="btn-close" onClick={onVolver}></button>
                     </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">{receta.name}</h5>
-                            <p className="card-text">{receta.ingredients}</p>
-                            <p className="card-text">{receta.instructions}</p>
+
+           
+                    <div className="modal-body">
+                        <div className="row">
+                            <div className="col-md-4">
+                                <img src={receta.image} className="img-fluid rounded" alt={receta.name}/>
+                            </div>
+
+                            <div className="col-md-8">
+                                <p>
+                                    <strong>Ingredientes:</strong><br />
+                                    {receta.ingredients}
+                                </p>
+
+                                <p>
+                                    <strong>Instrucciones:</strong><br />
+                                    {receta.instructions}
+                                </p>
+                            </div>
                         </div>
-                        <span onClick={onVolver}>x</span>
                     </div>
+
+           
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary" onClick={onVolver}> Cerrar</button>
+                    </div>
+
                 </div>
             </div>
         </div>
-    
-    )
-}
+    );
+};
+
 
 export default Card2;
